bf068b06b2ad287ad47684b34ef6d391f2d7dd33
Jaster Starý <jasterstary@gmail.com>
2024-03-08 22:55:46 +0100

    initial commit

---
+++ pkg/dependency.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'js' => [
+    //'/tingle/tingle-master-2019/dist/tingle.js',
+    NODE . '/tingle.js/dist/tingle.js',
+    '/tingle/plugin.js',
+  ],
+  'css' => [
+    //'/tingle/tingle-master-2019/dist/tingle.css',
+    NODE . '/tingle.js/dist/tingle.css',
+    '/tingle/style.css',
+  ],
+];
---
+++ pkg/loader.js
@@ -0,0 +1,6 @@
+
+import tingle from './tingle-master-2020/src/tingle.js';
+window.tingle = tingle;
+import "./plugin.js";
+import "./tingle-master-2020/dist/tingle.css";
+import "./style.css";
